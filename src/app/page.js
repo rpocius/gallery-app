d4de0a32a27fa473aa1300c8e2bf3789b67d8634
Rai Pocius <pocius.raimondas@gmail.com>
2024-05-12 20:54:43 -0500

    Start work on images array

--- src/app/page.js
+++ src/app/page.js
@@ -2,94 +2,27 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  const photos = [
+  'https://unsplash.com/photos/a-single-red-flower-in-a-field-of-blue-flowers-3Lpd6Wo3LJk',
+  'https://unsplash.com/photos/a-group-of-houses-sitting-on-top-of-a-sandy-beach-QxB8Wri4vok',
+  'https://unsplash.com/photos/a-blurry-image-of-a-blue-and-pink-background-zjpsG_CMTmY',
+  'https://unsplash.com/photos/a-person-standing-on-the-side-of-a-road-at-night-k_oHn7XBhVA'
+];
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+function renderPhotos(array) {
+  return array.map((element, index) => (
+    <div key={index} className={styles.photo}>
+      <Image src={element} alt='Unsplash image' width={500} height={500} />
+    </div>
+  ));
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+return (
+  <main className={styles.main}>
+    <div className={styles.gallery}>
+      {renderPhotos(photos)}
+    </div>
+  </main>
+);
 }
